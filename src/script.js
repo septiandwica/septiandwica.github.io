fad0c8463256b072c389bb74dd4c53e3f4b574fd
Septian Dwi Cahyo <112248603+septiandwica@users.noreply.github.com>
2023-03-08 06:25:23 +0700

    update

--- src/script.js
+++ src/script.js
@@ -150,7 +150,7 @@ document.onkeydown = function (e){
     if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)){
         return false;
     }
-    if (e.ctrlKey && e.keyCode == "I".charCodeAt(0)){
+    if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)){
         return false;
     }
 }
