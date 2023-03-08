d56ce062184f2b3b38fd38e8c30b58bbf3fab764
Septian Dwi Cahyo <112248603+septiandwica@users.noreply.github.com>
2023-03-08 07:02:44 +0700

    update

--- src/script.js
+++ src/script.js
@@ -133,23 +133,19 @@ loadmore.addEventListener("click", () => {
 AOS.init();
 
 // disable inspect element or dev mode
-document.addEventListener("contextmenu", function(e){
-    e.preventDefault();
-});
-
-document.onkeydown = function (e){
-    if (event) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)){
+document.onkeydown = function (e) {
+  if (e.keyCode == 123) {
       return false;
   }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)){
-        return false;
-    }
-    if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)){
-        return false;
-    }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+      return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+      return false;
+  }
+  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+      return false;
+  }
 }
 
 // Scrollspy botstrap
