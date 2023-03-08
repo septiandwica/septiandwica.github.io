eed342084d6e88fa1727cd8f95ed2abad342f7cd
Septian Dwi Cahyo <112248603+septiandwica@users.noreply.github.com>
2023-03-08 08:23:24 +0700

    update

--- src/script.js
+++ src/script.js
@@ -133,17 +133,24 @@ loadmore.addEventListener("click", () => {
 AOS.init();
 
 // disable inspect element or dev mode
-document.onkeydown = function (e) {
-  if (e.keyCode == 123) {
+document.addEventListener("contextmenu", function(e){
+  e.preventDefault();
+});
+
+document.onkeydown = function (e){
+  if (event) {
       return false;
   }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)){
       return false;
   }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)){
       return false;
   }
-  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)){
+      return false;
+  }
+  if (e.ctrlKey && e.keyCode == "I".charCodeAt(0)){
       return false;
   }
 }
